b08b096826012dbd547e142fd161767168a8b07e
Achim Staebler <astaebler@splunk.com>
2020-04-23 12:18:04 +0200

    refactor deploy,release into external scripts

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -3,3 +3,15 @@
 HASH="$(git rev-parse HEAD)"
 MSG="$(git log -1 --pretty=format:'Docs: %s ({sha} via travis)' "$HASH")"
 export HASH MSG
+
+git config user.name "$GH_USER_NAME"
+git config user.email "$GH_USER_EMAIL"
+git remote set-url origin git@github.com:signalfx/app-dev-workshop.git
+
+git remote set-branches --add origin gh-pages
+git fetch origin && git fetch origin gh-pages:gh-pages
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+  echo "Pushing to github";
+  mkdocs gh-deploy -v --clean -m "$MSG" --remote-name origin
+  git push origin gh-pages;
+fi
