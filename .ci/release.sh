dcd77b5b359335c9c244275c64528901375a1fec
Achim Staebler <astaebler@splunk.com>
2020-04-23 12:28:33 +0200

    script cleanup

--- .ci/release.sh
+++ .ci/release.sh
@@ -5,15 +5,17 @@ RESULT=$(curl -s -w "\n%{http_code}\n" \
   -H "Authorization: token $api_key"  \
   -d @- <<'EOF'
 {
-  "tag_name":         "$TRAVIS_TAG"
+  "tag_name": "$TRAVIS_TAG"
 }
 EOF
 )
+
 if [ "$(tail -1 <<< "$RESULT")" != "201" ]; then
   echo FAILED
   echo "$RESULT"
   exit 1
 fi
+
 RELEASE_ID=$(sed -ne 's/^  "id": \(.*\),$/\1/p' <<< "$RESULT")
 if [[ -z "$RELEASE_ID" ]]; then
   echo FAILED
