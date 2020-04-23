8158230c9dc5e6623292f23d581e36ed6029a444
Achim Staebler <astaebler@splunk.com>
2020-04-23 12:17:21 +0200

    add release script, make it executable

---
+++ .ci/release.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+RESULT=$(curl -s -w "\n%{http_code}\n" \
+  "https://api.github.com/repos/$TRAVIS_REPO_SLUG/releases" \
+  -H "Authorization: token $api_token"  \
+  -d @- <<'EOF'
+{
+  "tag_name":         "$TRAVIS_TAG"
+}
+EOF
+)
+if [ "$(tail -1 <<< "$RESULT")" != "201" ]; then
+  echo FAILED
+  echo "$RESULT"
+  exit 1
+fi
+RELEASE_ID=$(sed -ne 's/^  "id": \(.*\),$/\1/p' <<< "$RESULT")
+if [[ -z "$RELEASE_ID" ]]; then
+  echo FAILED
+  echo "$RESULT"
+  exit 1
+fi
