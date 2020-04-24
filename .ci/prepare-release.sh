6963f0b4e066341eac972645d494e731584a18c3
Achim Staebler <astaebler@splunk.com>
2020-04-24 16:43:40 +0200

    automate git tag and release process

---
+++ .ci/prepare-release.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -o errexit   # Exit on most errors
+set -o errtrace  # Make sure any error trap is inherited
+set -o nounset   # Disallow expansion of unset variables
+set -o pipefail  # Use last non-zero exit code in a pipeline
+
+FLAVOUR=${1:-minor}
+CURRENT=$(git describe --abbrev=0 --match "v*")
+TAG=$(bumpversion \
+--parse "v?(?P<major>\d+)\.(?P<minor>\d+)" \
+--serialize "{major}.{minor}" \
+--current-version "$CURRENT" \
+--dry-run \
+--allow-dirty \
+--list "$FLAVOUR" |
+awk -F= '/new_version/ { print $2 }')
+
+awk "/versions:/ { print; print \"  - v$TAG\";next }1" mkdocs.yml > mkdocs.new.yml
+mv mkdocs.new.yml mkdocs.yml
+awk "/Previous versions of the workshop are also available:/ { print; print \"[v$TAG](https://signalfx.github.io/app-dev-workshop/v$TAG/)\";next }1" README.md > README.new.md
+mv README.new.md README.md
+
+git add README.md mkdocs.yml
+git commit -m "Releasing v$TAG"
+git tag -a "v$TAG" -m "Version $TAG"
+git push --follow-tags origin master
