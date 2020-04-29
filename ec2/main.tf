a5840a177d1d410b2b4f0c1caa3d6f55a0b232bf
Robert Castley <robert.castley@gmail.com>
2020-04-29 14:53:21 +0100

    Fixed syntax for 0.12

--- ec2/main.tf
+++ ec2/main.tf
@@ -6,5 +6,5 @@ resource "aws_instance" "app-dev-instance" {
   ami           = "ami-085925f297f89fce1"
   instance_type = "t2.micro"
   key_name      = var.key_name
-  user_data     = "${file("../cloud-init/k3s.yaml")}"
+  user_data     = file("../cloud-init/k3s.yaml")
 }
