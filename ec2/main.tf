551ec87b8ffcbcc819a2091254a2581ff8a321ea
Robert Castley <robert.castley@gmail.com>
2020-04-30 09:23:00 +0100

    Applied formatting

--- ec2/main.tf
+++ ec2/main.tf
@@ -4,8 +4,8 @@ provider "aws" {
 }
 
 resource "aws_security_group" "instance" {
-  name = "AppDev-WorkShop-SG"  
-  
+  name = "AppDev-WorkShop-SG"
+
   ingress {
     from_port   = 22
     to_port     = 22
@@ -15,15 +15,14 @@ resource "aws_security_group" "instance" {
 }
 
 resource "aws_instance" "app-dev-instance" {
-  count         = var.aws_instance_count
-  ami           = lookup(var.ami,var.aws_region)
-  instance_type = var.instance_type  
-#  key_name      = var.key_name
-  vpc_security_group_ids  = [aws_security_group.instance.id]
-  user_data     = file("../cloud-init/k3s.yaml")
-  
+  count                  = var.aws_instance_count
+  ami                    = lookup(var.ami, var.aws_region)
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  user_data              = file("../cloud-init/k3s.yaml")
+
   root_block_device {
-    volume_size           = "15"
+    volume_size = "15"
   }
 
   tags = {
@@ -31,3 +30,4 @@ resource "aws_instance" "app-dev-instance" {
   }
 
 }
+
--- ec2/variables.tf
+++ ec2/variables.tf
@@ -1,27 +1,24 @@
 variable "aws_instance_count" {
   description = "Instance Count"
 }
- 
+
 variable "ami" {
   type = map
 
   default = {
-    "us-east-1"         = "ami-085925f297f89fce1"
-    "us-west-1"         = "ami-0f56279347d2fa43e"
-    "eu-central-1"      = "ami-0e342d72b12109f91"
-    "eu-west-1"         = "ami-0dad359ff462124ca"
-    "ap-southeast-1"    = "ami-0dad359ff462124ca"
+    "us-east-1"      = "ami-085925f297f89fce1"
+    "us-west-1"      = "ami-0f56279347d2fa43e"
+    "eu-central-1"   = "ami-0e342d72b12109f91"
+    "eu-west-1"      = "ami-0dad359ff462124ca"
+    "ap-southeast-1" = "ami-0dad359ff462124ca"
   }
 }
 
 variable "aws_region" {
- description = "Region (us-east-1, us-west-1, eu-central-1, eu-west-1, ap-southeast-1)" 
+  description = "Region (us-east-1, us-west-1, eu-central-1, eu-west-1, ap-southeast-1)"
 }
 
 variable "instance_type" {
   default = "t2.large"
 }
 
-#variable "key_name" {
-#  description = "Provide a valid key name for the region "
-#}
