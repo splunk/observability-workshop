2efc28e0555959891c134f65527bfb5a06cfc3b5
Robert Castley <robert.castley@gmail.com>
2020-05-01 16:48:20 +0100

    terraform fmt

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -1,27 +1,27 @@
 variable "aws_instance_count" {
   description = "Instance Count (Usually 1)"
 }
- 
+
 variable "aws_region" {
-  description = "Provide the desired region (for example: us-west-2)" 
+  description = "Provide the desired region (for example: us-west-2)"
 }
 
 variable "instance_type" {
- description = "Select instance type required (1 = Hot R.O.D. 2 = Sock Shop)" 
+  description = "Select instance type required (1 = Hot R.O.D. 2 = Sock Shop)"
 }
 
 data "aws_ami" "latest-ubuntu" {
-most_recent = true
-owners = ["099720109477"] # This is the owner id of Canonical who owns the official aws ubuntu images
+  most_recent = true
+  owners      = ["099720109477"] # This is the owner id of Canonical who owns the official aws ubuntu images
 
   filter {
-      name   = "name"
-      values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
   }
 
   filter {
-      name   = "virtualization-type"
-      values = ["hvm"]
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
 variable "instance_type_aws" {
