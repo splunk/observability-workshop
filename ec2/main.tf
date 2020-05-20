b519b4c76890c0f193c1cb12e1a871bd5d5d4ca1
Robert Castley <robert.castley@gmail.com>
2020-05-20 14:56:03 +0100

    Allow access to Hot R.O.D. app from browser

--- ec2/main.tf
+++ ec2/main.tf
@@ -13,6 +13,13 @@ resource "aws_security_group" "instance" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
