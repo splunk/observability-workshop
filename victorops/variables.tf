3ca3b4588def5c789657815406a1446e05b70310
Robert Castley <robert.castley@gmail.com>
2020-05-21 20:51:28 +0100

    Simplified Terraform

--- victorops/variables.tf
+++ victorops/variables.tf
@@ -7,17 +7,46 @@ variable "realm" {
 }
 
 variable "sfx_prefix" {
-  type        = string
   description = "Detector Prefix"
-  default     = "[SFx]"
 }
 
 variable "sfx_vo_id" {
-  type        = string
   description = "SignalFx VictorOps Integration ID"
 }
 
 variable "routing_key" {
-  type        = string
   description = "VictorOps Routing Key"
+}
+
+variable "message_body" {
+  default = <<-EOF
+    {{#if anomalous}}
+        Rule "{{{ruleName}}}" in detector "{{{detectorName}}}" triggered at {{timestamp}}.
+    {{else}}
+        Rule "{{{ruleName}}}" in detector "{{{detectorName}}}" cleared at {{timestamp}}.
+    {{/if}}
+
+    {{#if anomalous}}
+      Triggering condition: {{{readableRule}}}
+    {{/if}}
+
+    {{#if anomalous}}
+      Signal value: {{inputs.A.value}}
+    {{else}}
+      Current signal value: {{inputs.A.value}}
+    {{/if}}
+
+    {{#notEmpty dimensions}}
+      Signal details: {{{dimensions}}}
+    {{/notEmpty}}
+
+    {{#if anomalous}}
+      {{#if runbookUrl}}
+        Runbook: {{{runbookUrl}}}
+      {{/if}}
+      {{#if tip}}
+        Tip: {{{tip}}}
+      {{/if}}
+    {{/if}}
+  EOF
 }
