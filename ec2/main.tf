75f849d3b351a0f77ee3658dd1c5977c3a6e10af
Pieter Hagen <phagen@signalfx.com>
2020-07-28 10:20:43 +0200

    shellinabox back to 6501-removed port 8082

--- ec2/main.tf
+++ ec2/main.tf
@@ -26,14 +26,7 @@ resource "aws_security_group" "instance" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  ingress {
-    from_port   = 8082
-    to_port     = 8082
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
+  
   ingress {
     from_port   = 6501
     to_port     = 6501
