da875a787df572027a9ab04d889fa980ee1d9bf0
Robert Castley <robert.castley@gmail.com>
2021-06-25 16:09:30 +0100

    Added readme

--- apm/microservices-demo/k8s/rum-config.sh
+++ apm/microservices-demo/k8s/rum-config.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 if [ -z ${RUM_TOKEN+x} ]; then echo "RUM_TOKEN is unset. Please export RUM_TOKEN=YOUR_RUM_TOKEN"; fi
-envsubst '${REALM},${RUM_TOKEN},${INSTANCE}' < deployment-host.yaml > deploy.yaml
+envsubst '${REALM},${RUM_TOKEN},${INSTANCE}' < deployment-host.yaml > deployment.yaml
