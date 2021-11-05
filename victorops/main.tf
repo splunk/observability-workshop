822628c4e66e0d5f9f790120fd50ef48c7eb63aa
Geoff Higginbottom <github@geoffh.co.uk>
2021-11-05 13:54:17 +0000

    Update main.tf

--- victorops/main.tf
+++ victorops/main.tf
@@ -8,7 +8,7 @@ resource "signalfx_detector" "cpu_greater_90" {
   description  = "Alerts when CPU usage is greater than 90%"
   program_text = <<-EOF
     from signalfx.detectors.against_recent import against_recent
-    A = data('cpu.utilization', filter=filter('host', '${var.sfx_prefix}*')).publish(label='A')
+    A = data('cpu.utilization', filter=filter('aws_tag_Name', '${var.sfx_prefix}')).publish(label='A')
     detect(when(A > threshold(90))).publish('CPU utilization is greater than 90%')
   EOF
   rule {
