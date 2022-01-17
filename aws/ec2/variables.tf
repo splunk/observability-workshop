00970d5d965bf53cc470fd42770b1c39dbbb59b1
Achim Staebler <astaebler@splunk.com>
2022-01-17 09:28:04 +0100

    use slug to create project name, terraform workspace and tag aws resources

--- aws/ec2/variables.tf
+++ aws/ec2/variables.tf
@@ -6,6 +6,10 @@ variable "aws_region" {
   description = "Provide the desired region (for example: us-west-2)"
 }
 
+variable "slug" {
+  description = "Project name slug. Will be used to tag aws resources"
+}
+
 data "aws_ami" "latest-ubuntu" {
   most_recent = true
   owners      = ["099720109477"] # This is the owner id of Canonical who owns the official aws ubuntu images
