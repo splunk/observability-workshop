af4d37f35cbc2ffee36a6b8f59ceab53a3f3c8c2
Achim Staebler <astaebler@splunk.com>
2022-01-17 17:50:39 +0100

    use ubuntu 21.04

--- aws/ec2/variables.tf
+++ aws/ec2/variables.tf
@@ -16,7 +16,7 @@ data "aws_ami" "latest-ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-hirsute-21.04-amd64-server-*"]
   }
 
   filter {
