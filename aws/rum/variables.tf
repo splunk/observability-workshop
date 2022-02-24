461492148281f26c9e40c44a081b9cb0b98e8653
rcastley <rcastley@users.noreply.github.com>
2022-02-24 19:28:19 +0000

    Releasing v3.33

--- aws/rum/variables.tf
+++ aws/rum/variables.tf
@@ -1,6 +1,6 @@
 ### Workshop Variables ###
 variable "wsversion" {
-  default = "3.32" # only set to master for testing should normally be a specific version such as 2.42
+  default = "3.33" # only set to master for testing should normally be a specific version such as 2.42
 }
 variable "k9sversion" {
   default = "v0.24.14"
