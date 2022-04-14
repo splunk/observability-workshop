2bc2bded68c3253790c4120bec89b3874a41960e
p-hagen <hagenpcjm@gmailcom>
2022-04-14 15:18:37 +0200

    removed line nolonger required

--- workshop/apm/microservices-demo/k8s/rum-config.sh
+++ workshop/apm/microservices-demo/k8s/rum-config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ -z ${RUM_TOKEN+x} ]; then echo "RUM_TOKEN is unset. Please export RUM_TOKEN=YOUR_RUM_TOKEN"; fi
 envsubst '${REALM},${RUM_TOKEN},${INSTANCE}' < deployment-host.yaml > deploymentRUM.yaml
-sed -i 's/value: "0.75"/value: "0.99"/'  ./deploymentRUM.yaml
+#sed -i 's/value: "0.75"/value: "0.99"/'  ./deploymentRUM.yaml
 
   
