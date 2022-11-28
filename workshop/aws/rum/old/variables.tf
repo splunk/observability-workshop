6ed69418c33e7af6cbfee8eccf3a922c449f2b68
rcastley <rcastley@users.noreply.github.com>
2022-11-28 08:37:35 +0000

    Releasing v4.35

--- workshop/aws/rum/old/variables.tf
+++ workshop/aws/rum/old/variables.tf
@@ -1,6 +1,6 @@
 ### Workshop Variables ###
 variable "wsversion" {
-  default = "4.34" # only set to master for testing should normally be a specific version such as 2.42
+  default = "4.35" # only set to master for testing should normally be a specific version such as 2.42
 }
 variable "k9sversion" {
   default = "v0.24.14"
