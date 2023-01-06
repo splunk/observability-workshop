1c24e7a587d3c78f648113f500f4bba4beaf60d0
Robert Castley <robert.castley@gmail.com>
2023-01-06 12:48:34 +0000

    Output cloud-init

--- workshop/aws/ec2/outputs.tf
+++ workshop/aws/ec2/outputs.tf
@@ -1,6 +1,12 @@
-/* output "cloud-init" {
-  value = "\n${data.template_cloudinit_config.config.rendered}"
-} */
+output "user_data" {
+  value = templatefile("${path.module}/templates/userdata.yaml",
+    {
+     access_token = "${var.splunk_access_token}" 
+     rum_token = "${var.splunk_rum_token}" 
+     realm = "${var.splunk_realm}" 
+     presetup = "${var.splunk_presetup}"
+    } )
+}
 
 output "ip-addresses" {
   value = aws_instance.observability-instance.*.public_ip
