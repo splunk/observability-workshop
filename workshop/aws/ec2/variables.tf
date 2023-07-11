6bc6bc5b56673bedd0ab2b38211c856ce19f1e55
rcastley <rcastley@users.noreply.github.com>
2023-07-11 21:13:42 +0000

    Releasing v4.94

--- workshop/aws/ec2/variables.tf
+++ workshop/aws/ec2/variables.tf
@@ -25,7 +25,7 @@ variable "otel_demo" {
 variable "wsversion" {
   description = "Workshop version"
   type        = string
-  default     = "4.93"
+  default     = "4.94"
 }
 
 variable "aws_instance_count" {
