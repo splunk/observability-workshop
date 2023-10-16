84acd7e0ec41d476b585f5fcc86c3a277f81a276
rcastley <rcastley@users.noreply.github.com>
2023-10-16 17:33:50 +0000

    Releasing v5.8

--- multipass/main.tf
+++ multipass/main.tf
@@ -66,7 +66,7 @@ variable "otel_demo" {
 variable "wsversion" {
   description = "Workshop version"
   type        = string
-  default     = "5.7"
+  default     = "5.8"
 }
 
 variable "instance_password" {
