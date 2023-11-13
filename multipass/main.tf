29d57c84f02e4b34577132d599328a2969df8178
rcastley <rcastley@users.noreply.github.com>
2023-11-13 19:39:25 +0000

    Releasing v5.13

--- multipass/main.tf
+++ multipass/main.tf
@@ -72,7 +72,7 @@ variable "splunk_diab" {
 variable "wsversion" {
   description = "Workshop version"
   type        = string
-  default     = "5.12"
+  default     = "5.13"
 }
 
 variable "instance_password" {
