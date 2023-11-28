6aa891749a1c5ee1260811006c61c1ecb5765f5e
rcastley <rcastley@users.noreply.github.com>
2023-11-28 15:43:25 +0000

    Releasing v5.17

--- workshop/aws/ec2/variables.tf
+++ workshop/aws/ec2/variables.tf
@@ -99,7 +99,7 @@ variable "splunk_diab" {
 variable "wsversion" {
   description = "Workshop version"
   type        = string
-  default     = "5.16"
+  default     = "5.17"
 }
 
 data "aws_ami" "latest-ubuntu" {
