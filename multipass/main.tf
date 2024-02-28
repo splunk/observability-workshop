3059b1ed8a605cdc2c32b849010cfb9383133463
billg-splunk <billg-splunk@users.noreply.github.com>
2024-02-28 19:49:31 +0000

    Releasing v5.41

--- multipass/main.tf
+++ multipass/main.tf
@@ -72,7 +72,7 @@ variable "splunk_diab" {
 variable "wsversion" {
   description = "Workshop version"
   type        = string
-  default     = "5.40"
+  default     = "5.41"
 }
 
 variable "instance_password" {
--- workshop/aws/ec2/variables.tf
+++ workshop/aws/ec2/variables.tf
@@ -99,7 +99,7 @@ variable "splunk_diab" {
 variable "wsversion" {
   description = "Workshop version"
   type        = string
-  default     = "5.40"
+  default     = "5.41"
 }
 
 data "aws_ami" "latest-ubuntu" {
