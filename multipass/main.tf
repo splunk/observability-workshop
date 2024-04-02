e2c7ad8278476f60eb1729f74909c0721383d60b
swaretech <swaretech@users.noreply.github.com>
2024-04-02 20:43:26 +0000

    Releasing v5.45

--- multipass/main.tf
+++ multipass/main.tf
@@ -72,7 +72,7 @@ variable "splunk_diab" {
 variable "wsversion" {
   description = "Workshop version"
   type        = string
-  default     = "5.44"
+  default     = "5.45"
 }
 
 variable "instance_password" {
