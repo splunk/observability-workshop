1e690afae4a685abbea516df0a0404bb84540e92
Pieter Hagen <phagen@splunk.com>
2024-04-15 14:25:34 +0200

    ignore the changes to orbstack.sh

--- orbstack/orbstack.sh
+++ orbstack/orbstack.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-export ACCESS_TOKEN=
-export REALM=
-export RUM_TOKEN=
-export HEC_TOKEN=
-export HEC_URL=
+export ACCESS_TOKEN=hOyyjQ-N0-cA1VyCkEKNYA
+export REALM=eu0
+export RUM_TOKEN=E_fENjAd3lAjW9QYUTbGRg
+export HEC_TOKEN=***REMOVED***
+export HEC_URL=https://http-inputs-o11y-workshop-eu0.splunkcloud.com:443/services/collector/event
 
 orb create -c orbstack.yaml -a amd64 ubuntu:jammy
 sleep 2
