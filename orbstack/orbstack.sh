adc1499ce76d0fd5bcd561faa1a9ad3ac3d3ea74
rcastley <rcastley@splunk.com>
2024-04-15 12:42:19 +0100

    Orbstack support

--- orbstack/orbstack.sh
+++ orbstack/orbstack.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-export ACCESS_TOKEN="wSgTADmdeTvrZBfHy3GPNA"
-export REALM="eu0"
-export RUM_TOKEN="XVZtGmYfvn2uQe9razgZbg"
-export HEC_TOKEN="***REMOVED***"
-export HEC_URL="https://http-inputs-o11y-workshop-eu0.splunkcloud.com:443/services/collector/event"
+export ACCESS_TOKEN=
+export REALM=
+export RUM_TOKEN=
+export HEC_TOKEN=
+export HEC_URL=
 
 orb create -c orbstack.yaml -a amd64 ubuntu:jammy
 ORBENV=ACCESS_TOKEN:REALM:RUM_TOKEN:HEC_TOKEN:HEC_URL orb -m ubuntu -u splunk ansible-playbook /home/splunk/orb-setup.yml
