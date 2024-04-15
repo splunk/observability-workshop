519f5abe93a6b52e3b00a16f588befeff1a6d1a6
rcastley <rcastley@splunk.com>
2024-04-15 13:39:51 +0100

    Added instance to script

--- orbstack/orbstack.sh
+++ orbstack/orbstack.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
-<<<<<<< HEAD
-export ACCESS_TOKEN=hOyyjQ-N0-cA1VyCkEKNYA
-export REALM=eu0
-export RUM_TOKEN=E_fENjAd3lAjW9QYUTbGRg
-export HEC_TOKEN=***REMOVED***
-export HEC_URL=https://http-inputs-o11y-workshop-eu0.splunkcloud.com:443/services/collector/event
-=======
 export ACCESS_TOKEN=
 export REALM=
 export RUM_TOKEN=
 export HEC_TOKEN=
 export HEC_URL=
 export INSTANCE=
->>>>>>> d88cc7d74 (Added instance env var)
 
 orb create -c orbstack.yaml -a amd64 ubuntu:jammy
 sleep 2
