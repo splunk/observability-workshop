6d5dad47b34f19eab446249a61b35f1ded128352
Pieter Hagen <phagen@splunk.com>
2024-04-18 10:59:54 +0200

    checking if repo exist

--- workshop/petclinic/scripts/push_env.sh
+++ workshop/petclinic/scripts/push_env.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# Define the path to the JavaScript file
-JS_FILE="/home/splunk/spring-petclinic-microservices/spring-petclinic-api-gateway/src/main/resources/static/scripts/env.js"
+# Define the path to the repository directory
+REPO_DIR="/home/splunk/spring-petclinic-microservices"
 
-# Create the directory if it doesn't exist
-mkdir -p "$(dirname "$JS_FILE")"
+# Check if the repository directory exists
+if [ -d "$REPO_DIR" ]; then
+    echo "Repository directory exists."
+    # Define the path to the JavaScript file
+    JS_FILE="$REPO_DIR/spring-petclinic-api-gateway/src/main/resources/static/scripts/env.js"
 
-# Write the content to the JavaScript file
-cat <<EOF > "$JS_FILE"
+    # Create the directory if it doesn't exist
+    mkdir -p "$(dirname "$JS_FILE")"
+
+    # Write the content to the JavaScript file
+    cat <<EOF > "$JS_FILE"
 env = {
   RUM_REALM: '$RUM_REALM',
   RUM_AUTH: '$RUM_AUTH',
@@ -16,4 +22,7 @@ env = {
 }
 EOF
 
-echo "JavaScript file generated at: $JS_FILE"
+    echo "JavaScript file generated at: $JS_FILE"
+else
+    echo "Error: Repository directory does not exist."
+fi
