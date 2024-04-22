71eff77d72d90515c5293fe1008b9a7db46c3471
dmitchsplunk <dmitchsplunk@users.noreply.github.com>
2024-04-22 18:44:16 +0000

    Releasing v5.50

--- workshop/aws/ec2/variables.tf
+++ workshop/aws/ec2/variables.tf
@@ -99,7 +99,7 @@ variable "tagging_workshop" {
 variable "wsversion" {
   description = "Workshop version"
   type        = string
-  default     = "5.49"
+  default     = "5.50"
 }
 
 data "aws_ami" "latest-ubuntu" {
