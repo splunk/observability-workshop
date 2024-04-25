f8a17dc31da6153c3148cd3d779066d684140b0f
dmitchsplunk <derekmitchell@splunk.com>
2024-04-25 15:39:26 -0700

    updated tagging scripts to work with ansible

--- workshop/tagging/0-deploy-collector-with-services.sh
+++ workshop/tagging/0-deploy-collector-with-services.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
 if [ ! -f /home/splunk/.helmok ]; then
-  helm repo add splunk-otel-collector-chart https://signalfx.github.io/splunk-otel-collector-chart
-  helm repo update
-  helm install splunk-otel-collector \
-  --set="splunkObservability.realm=$REALM" \
-  --set="splunkObservability.accessToken=$ACCESS_TOKEN" \
-  --set="clusterName=$INSTANCE-k3s-cluster" \
-  --set="environment=tagging-workshop-$INSTANCE" \
-  splunk-otel-collector-chart/splunk-otel-collector
 
   cd /home/splunk/workshop/tagging/
+  ./1-deploy-otel-collector.sh
   ./2-deploy-creditcheckservice.sh
   ./3-deploy-creditprocessorservice.sh
   ./4-deploy-load-generator.sh
