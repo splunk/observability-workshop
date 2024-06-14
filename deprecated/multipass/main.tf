a3aba397624e51c2456344e0804fa2e63faca85f
rcastley <rcastley@users.noreply.github.com>
2024-06-14 13:22:02 +0000

    Releasing v5.61

--- deprecated/multipass/main.tf
+++ deprecated/multipass/main.tf
@@ -72,7 +72,7 @@ variable "splunk_diab" {
 variable "wsversion" {
   description = "Workshop version"
   type        = string
-  default     = "5.60"
+  default     = "5.61"
 }
 
 variable "instance_password" {
