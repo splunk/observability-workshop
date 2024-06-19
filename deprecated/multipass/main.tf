7f1a09a1c633dd26c6ab0f79574023001f007aa5
rcastley <rcastley@users.noreply.github.com>
2024-06-19 17:25:58 +0000

    Releasing v5.63

--- deprecated/multipass/main.tf
+++ deprecated/multipass/main.tf
@@ -72,7 +72,7 @@ variable "splunk_diab" {
 variable "wsversion" {
   description = "Workshop version"
   type        = string
-  default     = "5.62"
+  default     = "5.63"
 }
 
 variable "instance_password" {
--- workshop/aws/ec2/variables.tf
+++ workshop/aws/ec2/variables.tf
@@ -99,7 +99,7 @@ variable "tagging_workshop" {
 variable "wsversion" {
   description = "Workshop version"
   type        = string
-  default     = "5.62"
+  default     = "5.63"
 }
 
 data "aws_ami" "latest-ubuntu" {
