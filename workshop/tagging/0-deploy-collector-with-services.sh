539b63a1b2810bc71ac663c976cb6478ed0f5077
Achim Staebler <astaebler@splunk.com>
2024-07-08 15:07:31 +0200

    shellcheck tagging scripts

--- workshop/tagging/0-deploy-collector-with-services.sh
+++ workshop/tagging/0-deploy-collector-with-services.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 if [ ! -f /home/splunk/.helmok ]; then
-
-  cd /home/splunk/workshop/tagging/
+  cd /home/splunk/workshop/tagging/ || exit
   ./1-deploy-otel-collector.sh
   ./2-deploy-creditcheckservice.sh
   ./3-deploy-creditprocessorservice.sh
   ./4-deploy-load-generator.sh
 
-  echo $INSTANCE > /home/splunk/.helmok
+  echo "$INSTANCE" > /home/splunk/.helmok
 fi
