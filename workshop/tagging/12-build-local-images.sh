7d7c7e1cfca8ef38b3221fed6f5ac3c132b87cc1
Achim Staebler <astaebler@splunk.com>
2024-07-16 16:00:25 +0200

    use version tags for local images

--- workshop/tagging/12-build-local-images.sh
+++ workshop/tagging/12-build-local-images.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-docker build -t credit-check-service:latest creditcheckservice-java
-docker build -t credit-check-service:latest creditcheckservice-java-with-tags
-docker build -t credit-check-service:latest creditcheckservice-py
-docker build -t credit-check-service:latest creditcheckservice-py-with-tags
+docker build -t credit-check-service:1.0 creditcheckservice-java
+docker build -t credit-check-service:1.1 creditcheckservice-java-with-tags
+docker build -t credit-check-service:1.0 creditcheckservice-py
+docker build -t credit-check-service:1.1 creditcheckservice-py-with-tags
 docker build -t credit-processor-service:latest creditprocessorservice
 docker build -t loadgenerator:latest loadgenerator
