acd9d2cc7dfe84c9f6ff828c5cf8d9d61fe25e95
Achim Staebler <astaebler@splunk.com>
2024-07-17 11:28:03 +0200

    add service IMPL support to build local images

--- workshop/tagging/12-build-local-images.sh
+++ workshop/tagging/12-build-local-images.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-docker build -t credit-check-service:1.0 creditcheckservice-java
-docker build -t credit-check-service:1.1 creditcheckservice-java-with-tags
-docker build -t credit-check-service:1.0 creditcheckservice-py
-docker build -t credit-check-service:1.1 creditcheckservice-py-with-tags
+IMPL="${1:-py}"
+docker build -t credit-check-service:1.0 "creditcheckservice-${IMPL}"
+docker build -t credit-check-service:1.1 "creditcheckservice-${IMPL}-with-tags"
+# docker build -t credit-check-service:1.0 creditcheckservice-py
+# docker build -t credit-check-service:1.1 creditcheckservice-py-with-tags
 docker build -t credit-processor-service:latest creditprocessorservice
 docker build -t loadgenerator:latest loadgenerator
