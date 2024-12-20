df913652661844f6351efeaaebf70e73d3756bc4
dmitchsplunk <derekmitchell@splunk.com>
2024-12-20 14:19:43 -0800

    added k8s scenarios to the docker-k8s-otel workshop

--- workshop/docker-k8s-otel/helloworld/Program.cs
+++ workshop/docker-k8s-otel/helloworld/Program.cs
@@ -1,26 +1,9 @@
-using Microsoft.AspNetCore.Mvc;
-
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-string Hello([FromServices]ILogger<Program> logger, string? name)
-{
-    string result;
-
-    if (string.IsNullOrEmpty(name))
-    {
-       logger.LogInformation("/hello endpoint invoked anonymously");
-       result = "Hello, World!";
-    }
-    else
-    {
-        logger.LogInformation("/hello endpoint invoked by {name}", name);
-        result = String.Format("Hello, {0}!", name);
-    }
-
-    return result;
-}
-
-app.MapGet("/hello/{name?}", Hello);
+app.MapControllers();
 
 app.Run();
