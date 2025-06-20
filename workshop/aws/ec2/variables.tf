389e51187773ea3d44397565508a02755b856f2b
rcastley <rcastley@users.noreply.github.com>
2025-06-20 09:07:16 +0000

    Releasing v5.94

--- workshop/aws/ec2/variables.tf
+++ workshop/aws/ec2/variables.tf
@@ -99,7 +99,7 @@ variable "tagging_workshop" {
 variable "wsversion" {
   description = "Workshop version"
   type        = string
-  default     = "5.93"
+  default     = "5.94"
 }
 
 data "aws_ami" "latest-ubuntu" {
