bb773d4b3463cb52bb1ff68de92e3e5fcbaf118a
rcastley <rcastley@users.noreply.github.com>
2025-07-01 16:50:09 +0000

    Docs: Releasing 5.95

---
+++ v5.95/versionindex.ja.js
@@ -0,0 +1,23 @@
+var relearn_versionindex = [
+  {
+    "baseURL": "",
+    "isAbs": false,
+    "url": "https://splunk.github.io/observability-workshop/latest/",
+    "value": "",
+    "version": "latest"
+  },
+  {
+    "baseURL": "",
+    "isAbs": false,
+    "url": "https://splunk.github.io/observability-workshop/v5.94",
+    "value": "",
+    "version": "v5.94"
+  },
+  {
+    "baseURL": "",
+    "isAbs": false,
+    "url": "https://splunk.github.io/observability-workshop/v5.95",
+    "value": "",
+    "version": "v5.95"
+  }
+]
