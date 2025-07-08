11f551f013c8e11200019286b3bb05d1fe66cd65
dmitchsplunk <dmitchsplunk@users.noreply.github.com>
2025-07-08 22:05:02 +0000

    Releasing v5.98

--- workshop/aws/ec2/variables.tf
+++ workshop/aws/ec2/variables.tf
@@ -99,7 +99,7 @@ variable "tagging_workshop" {
 variable "wsversion" {
   description = "Workshop version"
   type        = string
-  default     = "5.97"
+  default     = "5.98"
 }
 
 data "aws_ami" "latest-ubuntu" {
