2f8a1e57bf521f34652e28d9fc3b5afc21aa8380
rcastley <rcastley@users.noreply.github.com>
2025-09-29 11:42:53 +0000

    Releasing v6.6

--- local-hosting/multipass/main.tf
+++ local-hosting/multipass/main.tf
@@ -52,7 +52,7 @@ variable "tagging_workshop" {
 variable "wsversion" {
   description = "Workshop version"
   type        = string
-  default     = "6.5"
+  default     = "6.6"
 }
 
 variable "user_data_tpl" {
