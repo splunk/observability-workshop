125e5e76d9c478e75da3aae3c38c6d00e6ca892c
Robert Castley <robert.castley@gmail.com>
2025-09-30 11:52:23 +0100

    Added additional params to k3d install

--- local-hosting/proxmox/ubuntu-cloud-k3d.sh
+++ local-hosting/proxmox/ubuntu-cloud-k3d.sh
@@ -193,7 +193,7 @@ runcmd:
   - curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
   # Create 3 node k3d cluster
-  - k3d cluster create ${HOSTNAME}-cluster --agents 2 --image rancher/k3s:v1.33.4-k3s1 --port "81:80@loadbalancer"
+  - k3d cluster create ${HOSTNAME}-cluster --agents 2 --agents-memory 6G --k3s-arg "--kubelet-arg=eviction-hard=memory.available<1Gi@server:0" --k3s-arg "--kubelet-arg=eviction-hard=memory.available<1Gi@agent:*" --image rancher/k3s:v1.33.4-k3s1 --port "81:80@loadbalancer" --port "82:82@loadbalancer" -v /var/log/syslog:/var/log/syslog -v /var/log/auth.log:/var/log/auth.log
 
   # Add user splunk to docker group
   - usermod -aG docker splunk
