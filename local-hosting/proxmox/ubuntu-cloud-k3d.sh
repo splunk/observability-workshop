e863494f5712d4855ea243a72e27e3fd8a476252
Robert Castley <robert.castley@gmail.com>
2025-10-10 09:29:29 +0100

    Bump chaos mesh version

--- local-hosting/proxmox/ubuntu-cloud-k3d.sh
+++ local-hosting/proxmox/ubuntu-cloud-k3d.sh
@@ -207,7 +207,7 @@ runcmd:
   - /snap/bin/kubectl apply -f /home/splunk/workshop/k3s/registry/registry.yaml
 
   # Chaos Mesh
-  - curl -sSL https://mirrors.chaos-mesh.org/v2.7.1/install.sh | bash -s -- --k3s
+  - curl -sSL https://mirrors.chaos-mesh.org/v2.8.0/install.sh | bash -s -- --k3s
 
   # Deploy Splunk secrets
   - /snap/bin/kubectl apply -f /tmp/workshop-secrets.yaml
