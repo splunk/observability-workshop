4b0443155501c9a103568dc17029265e4da6603b
Igor Olszewski <iolszewski@soldevelo.com>
2025-11-14 09:00:32 +0100

    Switch Kafka Helm chart from Bitnami to SolDevelo

--- workshop/demos/otel-discovery-demo/kubernetes/01_install_kafka.sh
+++ workshop/demos/otel-discovery-demo/kubernetes/01_install_kafka.sh
@@ -3,5 +3,5 @@
 echo Installing Kafka using helm
 
 # Note: disable SASL authentication on the client listener, to make it simpler for our demo application to connect
-# Reference:  https://github.com/bitnami/charts/blob/main/bitnami/kafka/README.md
-helm install kafka-release oci://registry-1.docker.io/bitnamicharts/kafka --set="listeners.client.protocol=PLAINTEXT"
+# Reference:  https://github.com/SolDevelo/charts/blob/main/README.md
+helm install kafka-release oci://registry-1.docker.io/soldevelo/kafka-chart --set="listeners.client.protocol=PLAINTEXT"
