fd21916f3aaec7d7841ac0ca5751b28be916b56d
dmitchsplunk <dmitchsplunk@users.noreply.github.com>
2026-02-20 19:00:59 +0000

    Releasing v6.38

--- local-hosting/multipass/main.tf
+++ local-hosting/multipass/main.tf
@@ -52,7 +52,7 @@ variable "tagging_workshop" {
 variable "wsversion" {
   description = "Workshop version"
   type        = string
-  default     = "6.37"
+  default     = "6.38"
 }
 
 variable "user_data_tpl" {
