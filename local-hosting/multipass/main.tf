4b63b752b1316c065c40e1e4edf8566e66560c83
dmitchsplunk <dmitchsplunk@users.noreply.github.com>
2026-02-24 13:53:13 +0000

    Releasing v6.40

--- local-hosting/multipass/main.tf
+++ local-hosting/multipass/main.tf
@@ -52,7 +52,7 @@ variable "tagging_workshop" {
 variable "wsversion" {
   description = "Workshop version"
   type        = string
-  default     = "6.39"
+  default     = "6.40"
 }
 
 variable "user_data_tpl" {
--- workshop/aws/ec2/variables.tf
+++ workshop/aws/ec2/variables.tf
@@ -99,7 +99,7 @@ variable "tagging_workshop" {
 variable "wsversion" {
   description = "Workshop version"
   type        = string
-  default     = "6.39"
+  default     = "6.40"
 }
 
 data "aws_ami" "latest-ubuntu" {
