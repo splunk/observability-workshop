c51ab030ddce8264bc5a357c5d2525876e5886f3
swaretech <swaretech@users.noreply.github.com>
2026-03-28 19:32:03 +0000

    Releasing v6.42

--- workshop/aws/ec2/variables.tf
+++ workshop/aws/ec2/variables.tf
@@ -99,7 +99,7 @@ variable "tagging_workshop" {
 variable "wsversion" {
   description = "Workshop version"
   type        = string
-  default     = "6.41"
+  default     = "6.42"
 }
 
 data "aws_ami" "latest-ubuntu" {
