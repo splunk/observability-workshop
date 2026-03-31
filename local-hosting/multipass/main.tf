39558e8b368562e325568ada66a19b71516ee00b
rcastley <rcastley@users.noreply.github.com>
2026-03-31 13:42:15 +0000

    Releasing v6.44

--- local-hosting/multipass/main.tf
+++ local-hosting/multipass/main.tf
@@ -52,7 +52,7 @@ variable "tagging_workshop" {
 variable "wsversion" {
   description = "Workshop version"
   type        = string
-  default     = "6.43"
+  default     = "6.44"
 }
 
 variable "user_data_tpl" {
--- workshop/aws/ec2/variables.tf
+++ workshop/aws/ec2/variables.tf
@@ -99,7 +99,7 @@ variable "tagging_workshop" {
 variable "wsversion" {
   description = "Workshop version"
   type        = string
-  default     = "6.43"
+  default     = "6.44"
 }
 
 data "aws_ami" "latest-ubuntu" {
