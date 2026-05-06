d9b66928c565629c012f30101c50a7727c7dd9cd
timhard-splunk <timhard-splunk@users.noreply.github.com>
2026-05-06 17:12:21 +0000

    Releasing v6.57

--- workshop/aws/ec2/variables.tf
+++ workshop/aws/ec2/variables.tf
@@ -99,7 +99,7 @@ variable "tagging_workshop" {
 variable "wsversion" {
   description = "Workshop version"
   type        = string
-  default     = "6.56"
+  default     = "6.57"
 }
 
 data "aws_ami" "latest-ubuntu" {
