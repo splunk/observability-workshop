f0cd2cb7e72107c20467fea4af5e844025256154
chambear2809 <chambear2809@users.noreply.github.com>
2026-05-08 21:50:32 +0000

    Releasing v6.61

--- workshop/aws/ec2/variables.tf
+++ workshop/aws/ec2/variables.tf
@@ -99,7 +99,7 @@ variable "tagging_workshop" {
 variable "wsversion" {
   description = "Workshop version"
   type        = string
-  default     = "6.60"
+  default     = "6.61"
 }
 
 data "aws_ami" "latest-ubuntu" {
