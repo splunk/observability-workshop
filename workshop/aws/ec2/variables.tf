cdba0c7beaddc95a7b33d7a28ce89d3e5b3e67ef
billg-splunk <billg-splunk@users.noreply.github.com>
2026-06-08 03:55:36 +0000

    Releasing v6.90

--- workshop/aws/ec2/variables.tf
+++ workshop/aws/ec2/variables.tf
@@ -99,7 +99,7 @@ variable "tagging_workshop" {
 variable "wsversion" {
   description = "Workshop version"
   type        = string
-  default     = "6.89"
+  default     = "6.90"
 }
 
 data "aws_ami" "latest-ubuntu" {
