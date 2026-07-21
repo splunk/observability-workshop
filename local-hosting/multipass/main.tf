12bcb468f3a92cf8b999fa6ab1039cc5b68e60b9
dmitchsplunk <dmitchsplunk@users.noreply.github.com>
2026-07-21 17:32:04 +0000

    Releasing v6.107

--- local-hosting/multipass/main.tf
+++ local-hosting/multipass/main.tf
@@ -52,7 +52,7 @@ variable "tagging_workshop" {
 variable "wsversion" {
   description = "Workshop version"
   type        = string
-  default     = "6.106"
+  default     = "6.107"
 }
 
 variable "user_data_tpl" {
