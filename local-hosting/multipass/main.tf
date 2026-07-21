388874731247eda8cd9bd364df60a12b2ac7c34a
swaretech <swaretech@users.noreply.github.com>
2026-07-21 15:52:05 +0000

    Releasing v6.106

--- local-hosting/multipass/main.tf
+++ local-hosting/multipass/main.tf
@@ -52,7 +52,7 @@ variable "tagging_workshop" {
 variable "wsversion" {
   description = "Workshop version"
   type        = string
-  default     = "6.105"
+  default     = "6.106"
 }
 
 variable "user_data_tpl" {
