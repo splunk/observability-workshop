d64e23196e5fb3994d90aac9d6bfa679a5ae84c6
hagen-p <hagen-p@users.noreply.github.com>
2026-07-29 13:52:40 +0000

    Releasing v6.109

--- local-hosting/multipass/main.tf
+++ local-hosting/multipass/main.tf
@@ -52,7 +52,7 @@ variable "tagging_workshop" {
 variable "wsversion" {
   description = "Workshop version"
   type        = string
-  default     = "6.108"
+  default     = "6.109"
 }
 
 variable "user_data_tpl" {
