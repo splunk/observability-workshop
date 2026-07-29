5f630a2bbce5e54542455f975c0e4d6945ed3a02
hagen-p <hagen-p@users.noreply.github.com>
2026-07-29 11:55:35 +0000

    Releasing v6.108

--- workshop/aws/ec2/variables.tf
+++ workshop/aws/ec2/variables.tf
@@ -99,7 +99,7 @@ variable "tagging_workshop" {
 variable "wsversion" {
   description = "Workshop version"
   type        = string
-  default     = "6.107"
+  default     = "6.108"
 }
 
 data "aws_ami" "latest-ubuntu" {
