1d047e60f497c15fcf3bed4ac923fbdcf30dc1ca
hagen-p <hagen-p@users.noreply.github.com>
2026-08-03 12:24:35 +0000

    Releasing v6.114

--- local-hosting/multipass/main.tf
+++ local-hosting/multipass/main.tf
@@ -52,7 +52,7 @@ variable "tagging_workshop" {
 variable "wsversion" {
   description = "Workshop version"
   type        = string
-  default     = "6.113"
+  default     = "6.114"
 }
 
 variable "user_data_tpl" {
--- workshop/aws/ec2/variables.tf
+++ workshop/aws/ec2/variables.tf
@@ -99,7 +99,7 @@ variable "tagging_workshop" {
 variable "wsversion" {
   description = "Workshop version"
   type        = string
-  default     = "6.113"
+  default     = "6.114"
 }
 
 data "aws_ami" "latest-ubuntu" {
