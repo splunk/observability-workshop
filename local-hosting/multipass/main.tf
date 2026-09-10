870e36c030055b6980b2858f6d157b6d93979fe6
dmitchsplunk <dmitchsplunk@users.noreply.github.com>
2026-09-10 17:38:16 +0000

    Releasing v6.128
    Reason: re-numbering the AI workshops

--- local-hosting/multipass/main.tf
+++ local-hosting/multipass/main.tf
@@ -52,7 +52,7 @@ variable "tagging_workshop" {
 variable "wsversion" {
   description = "Workshop version"
   type        = string
-  default     = "6.127"
+  default     = "6.128"
 }
 
 variable "user_data_tpl" {
