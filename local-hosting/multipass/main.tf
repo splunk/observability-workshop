3c63c2f47a35e8715b902c56d9f7f0061f25da60
rcastley <rcastley@users.noreply.github.com>
2026-09-15 07:30:43 +0000

    Releasing v6.133
    Reason: OBS1184 .conf26 fixes

--- local-hosting/multipass/main.tf
+++ local-hosting/multipass/main.tf
@@ -52,7 +52,7 @@ variable "tagging_workshop" {
 variable "wsversion" {
   description = "Workshop version"
   type        = string
-  default     = "6.132"
+  default     = "6.133"
 }
 
 variable "user_data_tpl" {
