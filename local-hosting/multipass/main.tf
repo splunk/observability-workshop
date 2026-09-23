73036f4cbea5b04713f2855d78e90b7214849e13
timhard-splunk <timhard-splunk@users.noreply.github.com>
2026-09-23 23:26:06 +0000

    Releasing v6.134
    Reason: ITSI 5.0 Workshop

--- local-hosting/multipass/main.tf
+++ local-hosting/multipass/main.tf
@@ -52,7 +52,7 @@ variable "tagging_workshop" {
 variable "wsversion" {
   description = "Workshop version"
   type        = string
-  default     = "6.133"
+  default     = "6.134"
 }
 
 variable "user_data_tpl" {
